2aea5f11577b6bcb470af514cc2fce3eb672aa9e
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-04-14 13:31:13 +0300

    update registration

--- deli_api/serializers.py
+++ deli_api/serializers.py
@@ -5,41 +5,28 @@ from django.contrib.auth.password_validation import validate_password
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-            required=True,
-            validators=[UniqueValidator(queryset=User.objects.all())]
-            )
-
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
+        fields = ['email', 'username', 'password', 'password2']
+      
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+    def save(self):
+        account = User(
+            email = self.validated_data['email'],
+            username= self.validated_data['username'],
         )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
 
-        
-        user.set_password(validated_data['password'])
-        user.save()
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        account.set_password(password)
+        account.save()
+        return account
 
-        return user
 
 
 class LoginSerializer(serializers.HyperlinkedModelSerializer):
