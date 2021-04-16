1d4b54a2b51cf79333081f6d4edddf47f3f30ca7
BwanaQ <thunjawax@gmail.com>
2021-04-16 06:39:57 +0300

    add order app

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'dish',
+    'order',
 ]
 
 
--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('dish.urls')),
+    path('api/v1/', include('order.urls')),
 
 
 
