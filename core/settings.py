46ae2fdfe4c98fcefd5a97f0d5753c707f45c7e8
BwanaQ <thunjawax@gmail.com>
2021-04-19 15:36:49 +0300

    heroku deploy  change allowed hosts to all

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['oly1-fast-food-api.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
